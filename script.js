21d92be267e076a61a8ab40a084b836902842f5a
Sander de Veen <sander.de.veen@dop.nu>
2022-02-07 14:23:48 +0100

    progress more colors

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-const container = document.querySelector('.container');
-const color1 = document.querySelector('.color-1');
-const color2 = document.querySelector('.color-2');
-const color3 = document.querySelector('.color-3');
+// const container = document.querySelector('.container');
+// const color1 = document.querySelector('.color-1');
+// const color2 = document.querySelector('.color-2');
+// const color3 = document.querySelector('.color-3');
 
-color2.addEventListener('click', () => {
-  container.classList.add('change')
-});
+// color2.addEventListener('click', () => {
+//   container.classList.add('change')
+// });
+
+// color1.addEventListener('click', () => {
+//   container.classList.remove('change')
+// });
+
+
+// get background color with js 
+
+// const option1 = document.write(document.getElementById("option-1").style.backgroundColor)
+// const option2 = document.getElementById("option-2")
+// const option3 = document.getElementById("option-3").style.backgroundColor
+// const option4 = document.getElementById("option-4").style.backgroundColor
+
+// console.log(option1);
+// console.log(option2);
+
+// console.log(option3);
+// console.log(option4);
+
+// document.write(document.getElementById("option-1").style.backgroundColor)
+
+// function checkColor(backgroundColor) {
+//   document.querySelector(background).style.background = 'orange';
+// }
+
+const color1 = document.querySelector('.color-1');
 
 color1.addEventListener('click', () => {
-  container.classList.remove('change')
+  document.getElementById("option-1").style.backgroundColor
 });
